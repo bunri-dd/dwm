7517010ad0f5bb82954116e1f173d9665691810c
Peter Frnco <Pet0o.frnco@gmail.com>
2022-11-23 22:30:08 +0100

    Apply swallow

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Mononoki Nerd Font:size=10" };
@@ -44,15 +45,17 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 6,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Steam",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Code - OSS",  NULL,       NULL,       1 << 3,       0,           -1 },
- 	{ NULL,		  NULL,		"spterm",		SPTAG(0),		1,			 -1 },
- 	{ NULL,		  NULL,		"spfm",		SPTAG(1),		1,			 -1 },
- 	{ NULL,		  NULL,	"keepassxc",		SPTAG(2),		0,			 -1 },
+ 	/* class         instance  title            tags mask  isfloating  isterminal  noswallow  monitor */
+ 	{ "Gimp",        NULL,     NULL,            1 << 6,    0,          0,           0,        -1 },
+ 	{ "Firefox",     NULL,     NULL,            1 << 1,    0,          0,          -1,        -1 },
+ 	{ "Alacritty",   NULL,     NULL,            0,         0,          1,           0,        -1 },
+	{ "discord",     NULL,     NULL,            1 << 2,    0,          0,           0,        -1 },
+	{ "Steam",       NULL,     NULL,            1 << 8,    0,          0,           0,        -1 },
+	{ "Code - OSS",  NULL,     NULL,            1 << 3,    0,          0,           0,        -1 },
+ 	{ NULL,          NULL,     "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
+ 	{ NULL,		       NULL,		 "spterm",		    SPTAG(0),	 1,			     1,           0,        -1 },
+ 	{ NULL,		       NULL,		 "spfm",		      SPTAG(1),	 1,			     1,           0,        -1 },
+ 	{ NULL,		       NULL,	   "keepassxc",		  SPTAG(2),	 0,			     0,           0,        -1 },
 
 };
 
